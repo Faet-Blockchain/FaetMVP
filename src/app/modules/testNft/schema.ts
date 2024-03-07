9dc7180d7ddad1d5a363f1742eea752fcf41a766
Suepaphly <tabenterprise@gmail.com>
2024-03-07 02:30:55 -0600

    Update schema.ts

--- src/app/modules/testNft/schema.ts
+++ src/app/modules/testNft/schema.ts
@@ -1,17 +1,3 @@
-/*
- * Copyright © 2023 Lisk Foundation
- *
- * See the LICENSE file at the top-level directory of this distribution
- * for licensing information.
- *
- * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
- * no part of this software, including this file, may be copied, modified,
- * propagated, or distributed except according to the terms contained in the
- * LICENSE file.
- *
- * Removal or modification of this copyright notice is prohibited.
- */
-
 import {
 	LENGTH_COLLECTION_ID,
 	LENGTH_NFT_ID,
@@ -50,7 +36,7 @@ export const mintNftParamsSchema = {
 						fieldNumber: 1,
 					},
 					attributes: {
-						dataType: 'bytes',
+						dataType: 'string',
 						fieldNumber: 2,
 					},
 				},
